e8c4406e55c7407733a3d0d40b56e9fe840ca485
Ido Yuster <45263827+SpiderPigXL@users.noreply.github.com>
2022-06-25 18:45:32 +0300

    Update move_semantics2.rs

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -4,9 +4,8 @@
 
 
 fn main() {
-    let vec0 = Vec::new();
-
-    let mut vec1 = fill_vec(&vec0);
+    let vec0: Vec<i32> = Vec::new();
+    let mut vec1 = fill_vec(Vec::new());
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -16,8 +15,9 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
-    let mut vec = vec.clone();
+fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+    let mut vec = vec;
+
     vec.push(22);
     vec.push(44);
     vec.push(66);
